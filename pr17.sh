c3b52f85813c42d001629728f5ba6747ac415180
rexzzyphn <rexzycrypto9@gmail.com>
2025-12-28 00:55:17 +0800

    Add files via upload

---
+++ pr17.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+REMOTE_PATH="/var/www/pterodactyl/resources/views/layouts/admin.blade.php"
+TIMESTAMP=$(date -u +"%Y-%m-%d-%H-%M-%S")
+BACKUP_PATH="${REMOTE_PATH}.bak_${TIMESTAMP}"
+
+echo "🚀 Menghilangkan Bar Node, Location, Database, Settings, Application Api, Mounts, Nest..."
+
+if [ -f "$REMOTE_PATH" ]; then
+  mv "$REMOTE_PATH" "$BACKUP_PATH"
+  echo "📦 Backup file lama dibuat di $BACKUP_PATH"
+fi
+
+cat > "$REMOTE_PATH" << 'EOF'
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>{{ config('app.name', 'Pterodactyl') }} - @yield('title')</title>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        <meta name="_token" content="{{ csrf_token() }}">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/favicons/manifest.json">
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
+        <link rel="shortcut icon" href="/favicons/favicon.ico">
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+        <meta name="theme-color" content="#0e4688">
+
+        @include('layouts.scripts')
+
+        @section('scripts')
+            {!! Theme::css('vendor/select2/select2.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/bootstrap/bootstrap.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/adminlte/admin.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/adminlte/colors/skin-blue.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/sweetalert/sweetalert.min.css?t={cache-version}') !!}
+            {!! Theme::css('vendor/animate/animate.min.css?t={cache-version}') !!}
+            {!! Theme::css('css/pterodactyl.css?t={cache-version}') !!}
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        @show
+    </head>
+    <body class="hold-transition skin-blue fixed sidebar-mini">
+        <div class="wrapper">
+            <header class="main-header">
+                <a href="{{ route('index') }}" class="logo">
+                    <span>{{ config('app.name', 'Pterodactyl') }}</span>
+                </a>
+                <nav class="navbar navbar-static-top">
+                    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                </nav>
+            </header>
+
+            <aside class="main-sidebar">
+                <section class="sidebar">
+                    <ul class="sidebar-menu">
+                        <li class="header">BASIC ADMINISTRATION</li>
+
+                        <li>
+                            <a href="{{ route('admin.index') }}">
+                                <i class="fa fa-home"></i> <span>Overview</span>
+                            </a>
+                        </li>
+
+@if(Auth::user()->id == 1)
+                        <li>
+                            <a href="{{ route('admin.settings') }}">
+                                <i class="fa fa-wrench"></i> <span>Settings</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('admin.api.index') }}">
+                                <i class="fa fa-gamepad"></i> <span>Application API</span>
+                            </a>
+                        </li>
+
+                        <li class="header">MANAGEMENT</li>
+
+                        <li>
+                            <a href="{{ route('admin.databases') }}">
+                                <i class="fa fa-database"></i> <span>Databases</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('admin.locations') }}">
+                                <i class="fa fa-globe"></i> <span>Locations</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('admin.nodes') }}">
+                                <i class="fa fa-sitemap"></i> <span>Nodes</span>
+                            </a>
+                        </li>
+
+                        <li class="header">SERVICE MANAGEMENT</li>
+
+                        <li>
+                            <a href="{{ route('admin.mounts') }}">
+                                <i class="fa fa-magic"></i> <span>Mounts</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('admin.nests') }}">
+                                <i class="fa fa-th-large"></i> <span>Nests</span>
+                            </a>
+                        </li>
+@endif
+                    </ul>
+                </section>
+            </aside>
+
+            <div class="content-wrapper">
+                @yield('content')
+            </div>
+        </div>
+    </body>
+</html>
+EOF
+
+chmod 644 "$REMOTE_PATH"
+
+echo "✅ Berhasil Menghilangkan Bar Node, Location, Database, Settings, Application Api, Mounts, Nest!"
+echo "📂 Lokasi file: $REMOTE_PATH"
+echo "🗂️ Backup file lama: $BACKUP_PATH (jika sebelumnya ada)"
+echo "🔒 Hanya Admin (ID 1) yang bisa melihat Bar Node, Location, Database, Settings, Application Api, Mounts, Nest."
